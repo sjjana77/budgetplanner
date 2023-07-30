f6d8e12aaa2bb5a57b891c88a33d54d48cf29ce7
sjjana77 <janardh777@gmail.com>
2023-07-30 09:04:53 +0530

    Deployed Website

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route,Link} from "react-router-dom"
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Sourceofincome from "./components/Sourceofincome";
 import Budget from './components/Budget';
@@ -13,15 +13,15 @@ function App() {
   });
   return (
     <div className="App">
-       <BrowserRouter>
-       <UserProvider>
-       <Routes>
-        <Route path ="/" element={<Dashboard />}/>
-        <Route path ="/source" element={<Sourceofincome />}/>
-        <Route path ="/budget" element={<Budget />}/>
-        </Routes>
+      <Router>
+        <UserProvider>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/source" element={<Sourceofincome />} />
+            <Route path="/budget" element={<Budget />} />
+          </Routes>
         </UserProvider>
-       </BrowserRouter>
+      </Router>
     </div>
   );
 }
