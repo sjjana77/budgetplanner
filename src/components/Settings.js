0b45826a25acaf26f814ef3592f10719cd05e9f8
JANARDH K <67502853+sjjana77@users.noreply.github.com>
2023-08-28 10:17:22 +0000

    deployed website

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -12,31 +12,56 @@ const Settings = () =>{
     }); 
     const addsettings = (type) =>{
         if(type === "source"){
-            if(budget_details.source_options.includes(settingstmpvalue.source.toLowerCase())){
+            const lowerCaseSourceOptions = budget_details.source_options.map(source => source.toLowerCase());
+            if(lowerCaseSourceOptions.includes(settingstmpvalue.source.toLowerCase().trim())){
                 alert("Source already Present");
             }
             else{
                 let tmp = budget_details.source_options;
-                tmp.push(settingstmpvalue.source);
+                tmp.push(settingstmpvalue.source.trim());
                 setbudget_details({...budget_details,source_options:tmp});
             }
+            setsettingstmpvalue({...settingstmpvalue, source:''});
+        }
+        if(type === "saving"){
+            const lowerCaseSourceOptions = budget_details.budget_options.filter(option => option.type === 'S') 
+                                           .map(option => option.category.toLowerCase());
+            if(lowerCaseSourceOptions.includes(settingstmpvalue.saving.toLowerCase().trim())){
+                alert("Savings Type already Present");
+            }
+            else{
+                let tmp = budget_details.budget_options;
+                tmp.push({type:'S',category: settingstmpvalue.saving.trim()});
+                setbudget_details({...budget_details,budget_options:tmp});
+            }
+            setsettingstmpvalue({...settingstmpvalue, saving:''});
+        }
+        if(type === "expense"){
+            const lowerCaseSourceOptions = budget_details.budget_options.filter(option => option.type === 'E') 
+                                           .map(option => option.category.toLowerCase());
+            if(lowerCaseSourceOptions.includes(settingstmpvalue.expense.toLowerCase().trim())){
+                alert("Expense already Present");
+            }
+            else{
+                let tmp = budget_details.budget_options;
+                tmp.push({type:'E',category: settingstmpvalue.expense.trim()});
+                setbudget_details({...budget_details,budget_options:tmp});
+            }
+            setsettingstmpvalue({...settingstmpvalue, expense:''});
         }
     }
+    const renderAddButton = (type) => {
+        if (settingstmpvalue[type]) {
+          return (
+            <button className="btn btn-primary" onClick={() => addsettings(type)}>
+              Add
+            </button>
+          );
+        }
+        return null;
+    };
     const changehandler = (e) =>{
         setsettingstmpvalue({...settingstmpvalue,[e.target.name]:e.target.value});
-        if(e.target.value !== ""){
-            document.getElementsByClassName(e.target.name+"_col")[0].innerHTML = "";
-            const btn = document.createElement("button");
-            btn.className = "btn btn-primary";
-            btn.textContent = "Add";
-            btn.addEventListener("click", function() {
-                addsettings(e.target.name); 
-            });
-            document.getElementsByClassName(e.target.name+"_col")[0].appendChild(btn);
-        }
-        else{
-            document.getElementsByClassName(e.target.name+"_col")[0].innerHTML = "";
-        } 
     }
 
     if(budget_details.fontsize === undefined){
@@ -87,6 +112,7 @@ const Settings = () =>{
                     <textarea className='form-control' name='feedback' value={settingstmpvalue.feedback} id='feedback' onChange={changehandler} placeholder='Feedback'></textarea>
                 </div>
                 <div className='col feedback_col'>
+                {renderAddButton('feedback')}
                 </div>
             </div>
             <div className='row'>
@@ -97,6 +123,7 @@ const Settings = () =>{
                 <input className='form-control' value={settingstmpvalue.expense} id='expense' name='expense' onChange={changehandler} type="text" placeholder="Expense" />
                 </div>
                 <div className='col expense_col'>
+                {renderAddButton('expense')}
                 </div>
             </div>
             <div className='row'>
@@ -107,6 +134,7 @@ const Settings = () =>{
                 <input className='form-control' value={settingstmpvalue.saving} id='saving' name='saving' onChange={changehandler} type="text" placeholder="Saving" />
                 </div>
                 <div className='col saving_col'>
+                {renderAddButton('saving')}
                 </div>
             </div>
             <div className='row'>
@@ -117,7 +145,7 @@ const Settings = () =>{
                 <input className='form-control' value={settingstmpvalue.source} id='source' name='source' onChange={changehandler} type="text" placeholder="Income Source" />
                 </div>
                 <div className='col source_col'>
-                   
+                {renderAddButton('source')}
                 </div>
             </div>
         </div>
